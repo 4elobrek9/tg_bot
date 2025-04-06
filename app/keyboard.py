47ce84e24b09e01a00b14109e64c523c5f96eb99
brother <hacker@gmail.com>
2025-04-06 19:08:14 +0300

    mini edit

--- app/keyboard.py
+++ app/keyboard.py
@@ -1,17 +1,17 @@
-from aiogram.types import (
-    ReplyKeyboardMarkup,
-    KeyboardButton,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton
-)
+# from aiogram.types import (
+#     ReplyKeyboardMarkup,
+#     KeyboardButton,
+#     InlineKeyboardMarkup,
+#     InlineKeyboardButton
+# )
 
-main = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='324')],
-        [KeyboardButton(text='34')],
-        [KeyboardButton(text='24')],
-        [KeyboardButton(text='32')]
-    ],
-    resize_keyboard=True,
-    input_field_placeholder='выбирай пункт меню сука'
-)
+# main = ReplyKeyboardMarkup(
+#     keyboard=[
+#         [KeyboardButton(text='324')],
+#         [KeyboardButton(text='34')],
+#         [KeyboardButton(text='24')],
+#         [KeyboardButton(text='32')]
+#     ],
+#     resize_keyboard=True,
+#     input_field_placeholder='выбирай пункт меню сука'
+# )
